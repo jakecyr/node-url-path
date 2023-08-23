932aaaf4b1ddeaefab3831d60364b6b02139d7f0
Jake Cyr <cyrjake@gmail.com>
2023-08-23 00:14:54 -0400

    Remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-const { URLPath } = require('./dist/URLPath');
-const urlPath = new URLPath('https://google.com/api/?someExistingParams=1')
-  .joinPath('/query')
-  .addQueryParams({
-    query: "How to make URLs easier when working with Node",
-    apiKey: "1234567890",
-  });
-
-console.log(urlPath.href);
